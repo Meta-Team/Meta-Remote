85f51918441a1511a7da3ba266ffe83281f5296f
zikail2 <zikail2@illinois.edu>
2021-02-11 00:14:34 +0800

    Move files around and refactor.

---
+++ local_openocd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo "================ Meta-Remote Local ================"
+
+remote=$1
+command=$2
+elf=$3
+if [ -z ${remote+x} ] || [ -z ${command+x} ] || [ -z ${elf+x} ];then
+  echo "usage: $0 <remote IP> connect|flash|flash_connect <elf file>"
+  exit 1
+fi
+if [ "$command" != "connect" ] && [ "$command" != "flash" ] && [ "$command" != "flash_connect" ];then
+  echo "Invalid command ${command}. Expecting: connect, flash, or flash_connect."
+  exit 1
+fi
+if [ ! -f "$elf" ]; then
+  echo "${elf} not found."
+  exit 1
+fi
+
+echo "Command: $command"
+echo "ELF file: ${elf}"
+
+# Generate MD5
+md5=$(md5 -q "${elf}")
+echo "MD5: ${md5}"
+
+printf "Copying elf file to remote... "
+scp "${elf}" "${remote}:/tmp/meta.elf"
+printf "Done\n"
+
+echo -e "${BLUE}Invoking remote script...${NC}"
+ssh "$remote" "remote_openocd.sh ${command} /tmp/meta.elf ${md5}"
---
+++ remote_openocd.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+echo "================ Meta-Remote Remote ================"
+
+command=$1
+
+if [ -z ${command+x} ]; then
+  echo "No command is given!${NC}"
+  exit 1
+fi
+
+echo "Command:      ${command}"
+
+if [ "$command" == "connect" ]; then
+  cmd="gdb_port pipe"
+else
+  if [ "$command" == "flash" ]; then
+    cmd="program ${elf} verify reset exit"
+  elif [ "$command" == "flash_connect" ]; then
+    cmd="program ${elf} verify reset"
+  else
+    echo "Invalid command!"
+    exit 1
+  fi
+
+  # Get and check inputs
+  elf=$2
+  md5=$3
+  if [ -z ${elf+x} ]; then
+    echo "No elf file is given!"
+    exit 1
+  fi
+  if [ ! -f "$elf" ]; then
+    echo "No such file: ${elf}!"
+    exit 1
+  fi
+  if [ -z ${md5+x} ]; then
+    echo "No md5 is given!"
+    exit 1
+  fi
+
+  echo "ELF file:     ${elf}"
+  echo "Expected MD5: ${md5}"
+
+  # Check MD5
+  actual_md5="$(md5sum "$elf" | awk '{ print $1 }')"
+  echo "Actual MD5:   ${actual_md5}"
+  if [ "$md5" != "$actual_md5" ]; then
+    echo "Invalid elf file!"
+    exit 2
+  fi
+fi
+
+# Killing previous openocd (if any)
+pkill openocd
+
+openocd -f "${DIR}/rm_board_stlink.cfg" -c "${cmd}"
