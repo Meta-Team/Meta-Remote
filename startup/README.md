c5a83be2666614dfe8a3d3924450077c658d0ee1
liuzikai <zikai.liu@outlook.com>
2021-07-27 22:50:53 +0800

    Update auto startup.

--- startup/README.md
+++ startup/README.md
@@ -1,6 +1,6 @@
 # How to setup meta.service
 
-1. Clone the code and name the folder "zkr_test"
+1. Clone the code and name the folder "Meta-Remote/startup"
 2. Go into the folder, ```sudo cp meta.service /lib/systemd/system/```
 3. ```sudo systemctl daemon-reload```
 4. ```systemctl enable meta.service```
--- startup/manage.sh
+++ startup/manage.sh
@@ -3,13 +3,13 @@
 #echo meta | sudo -S systemctl stop nvgetty
 #echo meta | sudo -S systemctl disable nvgetty
 #udevadm trigger
-cd /home/meta/zkr_test/
+cd /home/meta/Meta-Remote/startup/
 whoami > rc.log
 pwd >> rc.log
 echo "$PATH" >> rc.log
 
 echo meta | sudo -S ./flush_serial /dev/ttyUSB0
 echo meta | sudo -S ./flush_serial /dev/ttyTHS1
-sudo python3 manager.py "socat -d /dev/ttyUSB0,b115200,raw,nonblock,ignoreeof,echo=0 TCP4-LISTEN:2021,reuseaddr" > manager.log &
+sudo python3 manager.py "socat -d /dev/ttyUSB0,b115200,raw,nonblock,ignoreeof,echo=0 TCP4-LISTEN:2021,reuseaddr" "/home/meta/Meta-Vision-Solais/cmake-build-jetson-debug/tools/Solais/Solais" > manager.log &
 
 exit 0
